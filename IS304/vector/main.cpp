42d4a8e7cbc504c4f55b2efb22649405841cecbb
Krosbift <TorifaOficial@gmail.com>
2023-08-25 14:41:18 -0500

    fix, this errors

--- IS304/vector/main.cpp
+++ IS304/vector/main.cpp
@@ -3,5 +3,12 @@
 
 int main() {
   vector<int> VECTOR;
+  for (size_t i = 0; i < 10; i++) {
+    VECTOR.push_back(i);
+  }
+  for (size_t i = 0; i < VECTOR.size(); i++) {
+    std::cout << VECTOR.at(i) << std::endl;
+  }
+  
   return 0;
 }
--- IS304/vector/vector-class/vector.hh
+++ IS304/vector/vector-class/vector.hh
@@ -59,14 +59,14 @@ class vector {
       * */
     vector& operator=(const vector &vector_copy) {
       if (this != &vector_copy) {
-        delete[] this.pointer_vector;
+        delete[] pointer_vector;
 
-        this.capacity = vector_copy.capacity;
-        this.elements = vector_copy.elements;
+        capacity = vector_copy.capacity;
+        elements = vector_copy.elements;
 
-        this.pointer_vector = new T[this.capacity];
-        for (size_t i = 0; i < this.elements; ++i) {
-          this.pointer_vector[i] = vector_copy.pointer_vector[i];
+        pointer_vector = new T[capacity];
+        for (size_t i = 0; i < elements; ++i) {
+          pointer_vector[i] = vector_copy.pointer_vector[i];
         }
       }
       return *this;
@@ -77,16 +77,16 @@ class vector {
       * @return el valor del elemento en la posición indicada. 
       * */
     T at(size_t index) const {
-      if (index >= this.elements) {
+      if (index >= elements) {
         throw std::out_of_range("Index out of range");
       }
-      return this.pointer_vector[index];
+      return pointer_vector[index];
     }
 
     /** Se encarga de devolver el total de elementos que existen en el vector:
       * */
     size_t size() const {
-      return this.elements;
+      return elements;
     }
 
     /** Se encarga de agregar un elemento al final del vector, agrega mas espacio de memoria si es necesario,
@@ -95,59 +95,59 @@ class vector {
       * @param value valor que se va a agregar al final del vector.
       * */
     void push_back(const T &value) {
-      if (this.elements == this.capacity) {
-        this.capacity += 100;
+      if (elements == capacity) {
+        capacity += 100;
         
-        T* new_pointer_vector = new T[this.capacity];
-        for (size_t i = 0; i < this.elements; ++i) {
-          new_pointer_vector[i] = this.pointer_vector[i];
+        T* new_pointer_vector = new T[capacity];
+        for (size_t i = 0; i < elements; ++i) {
+          new_pointer_vector[i] = pointer_vector[i];
         }
 
-        delete[] this.pointer_vector;
-        this.pointer_vector = new_pointer_vector;
+        delete[] pointer_vector;
+        pointer_vector = new_pointer_vector;
       }
-      this.pointer_vector[this.elements++] = value;
+      pointer_vector[elements++] = value;
     }
 
     /** Se encarga de eliminar el ultimo elemento del vector, si es necesario, reducir la capacidad del vector
       * de memoria que ocupa el vector para no usar mas memoria de la necesaria:
       * */
     void pop_back() {
-      if (!this.empty()) {
+      if (!empty()) {
         throw std::out_of_range("vector is empty");
       }
-      if ((this.elements + 10) == this.capacity) {
-        this.capacity -= 5;
+      if ((elements + 10) == capacity) {
+        capacity -= 5;
 
-        T* new_pointer_vector = new T[this.capacity];
-        for (size_t i = 0; i < (this.elements - 1); i++) {
-          new_pointer_vector[i] = this.pointer_vector[i];
+        T* new_pointer_vector = new T[capacity];
+        for (size_t i = 0; i < (elements - 1); i++) {
+          new_pointer_vector[i] = pointer_vector[i];
         }
 
-        delete[] this.pointer_vector;
-        this.pointer_vector = new_pointer_vector;
+        delete[] pointer_vector;
+        pointer_vector = new_pointer_vector;
       }
-      this.elements--;
+      elements--;
     }
 
     /** Elimina todos los elementos del vector, reseteando la capacidad del vector a 10 y el numero de elementos a 0:
       * */
       void clear() {
-        if (!this.empty()) {
+        if (!empty()) {
           throw std::out_of_range("vector is empty");
         }
-        this.capacity = 10;
-        this.elements = 0;
-        T* new_pointer_vector = new T[this.capacity];
+        capacity = 10;
+        elements = 0;
+        T* new_pointer_vector = new T[capacity];
 
-        delete[] this.pointer_vector;
-        this.pointer_vector = new_pointer_vector;
+        delete[] pointer_vector;
+        pointer_vector = new_pointer_vector;
       }
 
       /** Se encarga de devolver si el vector esta vacio o no:
         * */
       bool empty() const {
-        if (this.elements == 0){
+        if (elements == 0){
           return true;
         }
         return false;
