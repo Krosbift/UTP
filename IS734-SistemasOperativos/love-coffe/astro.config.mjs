cf8dab75bf1385159e5036a92c673f1cb96110ad
Krosbift <torifaoficial@gmail.com>
2025-05-30 20:54:05 -0500

    guardar mini proyecto de SO y actualizar los archivos de arquitectura

---
+++ IS734-SistemasOperativos/love-coffe/astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://Krosbift.github.io',
+  base: '/love-coffe/',
+});
